7d8784b371ebc174cace7181cf8c3985f6fce90d
Trickster <trickster1034@gmail.com>
2026-01-16 18:43:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,6 +35,8 @@ let lastTapTime = 0;
 let pipes = [], meteors = [], gameStarted = false, gameOver = false, gameWon = false;
 let gamePhase = 1, showScoreboard = false, endTriggered = false, showSkip = false;
 let particles = [];
+let damageTexts = [];
+
 
 // Storage
 let highScore = localStorage.getItem("highScore") || 0;
